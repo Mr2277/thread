07218665a728a8d5421b59826dd50a3ef4497354
Mr2277 <837544713@qq.com>
2018-04-26 14:48:20 +0800

    produceConsume

---
+++ src/main/java/com/test/two/one/ProduceThread.java
@@ -0,0 +1,14 @@
+package com.test.two.one;
+
+public class ProduceThread extends Thread{
+    private Produce p;
+    public ProduceThread(Produce p){
+        this.p = p;
+    }
+    @Override
+    public void run() {
+        while (true) {
+            p.produce();
+        }
+    }
+}
