fad0836208d7c8b17d9de76077d58a1bbf11e4e3
master <837544713@qq.com>
2018-05-06 08:32:48 +0800

    MyThread

---
+++ src/main/java/com/test/four/MyThread.java
@@ -0,0 +1,13 @@
+package com.test.four;
+
+public class MyThread extends Thread{
+    private MyService myService;
+    public MyThread(MyService myService){
+        this.myService=myService;
+    }
+
+    @Override
+    public void run() {
+        myService.testMethod();
+    }
+}
