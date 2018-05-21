9ad5f6622c0ee91a89cd33f1430b811a6067aa31
Mr2277 <837544713@qq.com>
2018-05-21 17:29:01 +0800

    同一个reetwritelock读写互斥

--- src/main/java/com/test/nine/Service.java
+++ src/main/java/com/test/nine/Service.java
@@ -1,24 +1,28 @@
 package com.test.nine;
 
+import java.util.concurrent.locks.Condition;
 import java.util.concurrent.locks.ReentrantReadWriteLock;
 
 public class Service {
     private ReentrantReadWriteLock lock=new ReentrantReadWriteLock();
+    private ReentrantReadWriteLock lock1=new ReentrantReadWriteLock();
+    //private ReentrantReadWriteLock lock1=new ReentrantReadWriteLock();
     public void write() throws InterruptedException {
         lock.writeLock().lock();
 
             System.out.println("获得读锁：" + Thread.currentThread().getName()
                     + " " + System.currentTimeMillis());
         lock.writeLock().unlock();
+        System.out.println("write end");
     }
    public void read() throws InterruptedException {
-        lock.readLock().lock();
+        lock1.readLock().lock();
 
        System.out.println("获得读锁：read" + Thread.currentThread().getName()
                + " " + System.currentTimeMillis());
        Thread.sleep(1000000);
 
-       lock.readLock().unlock();
-
+       lock1.readLock().unlock();
+       System.out.println("read end");
    }
 }
