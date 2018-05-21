5a876dceae5cb972b72f0164c10c18d068c66098
Mr2277 <837544713@qq.com>
2018-05-21 13:47:55 +0800

    公平锁

---
+++ src/main/java/com/test/seven/Service.java
@@ -0,0 +1,21 @@
+package com.test.seven;
+
+import java.util.concurrent.locks.ReentrantLock;
+
+public class Service {
+    private ReentrantLock lock ;
+
+    public Service(boolean isFair) {
+        lock = new ReentrantLock(isFair);
+    }
+
+    public void serviceMethod() {
+        try {
+            lock.lock();
+            System.out.println("ThreadName=" + Thread.currentThread().getName()
+                    + " 获得锁定");
+        } finally {
+            lock.unlock();
+        }
+    }
+}
