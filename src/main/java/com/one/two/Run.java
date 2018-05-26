8dbe1cc6bede0987aa9fda9c8370c90ac437066c
master <837544713@qq.com>
2018-05-26 12:36:16 +0800

    JVM

---
+++ src/main/java/com/one/two/Run.java
@@ -0,0 +1,9 @@
+package com.one.two;
+
+public class Run {
+    private static final int _1MB=1024*1024;
+    public static void main(String[] args){
+        byte[] a,b,c,d;
+        a=new byte[_1MB];
+    }
+}
