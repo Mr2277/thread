e5c994d29f2e8792fa769f8c21a94745ca40c23a
Mr2277 <837544713@qq.com>
2018-06-12 17:20:18 +0800

    HashMap自定义key

--- src/main/java/com/test/twelve/Run.java
+++ src/main/java/com/test/twelve/Run.java
@@ -1,25 +1,23 @@
 package com.test.twelve;
 
 public class Run {
-    public static int add(){
+    public static void add(){
         System.out.println("1、除法计算开始。");
         int result=0;
         try {
             int x = Integer.parseInt("0");
             int y = Integer.parseInt("0");
             result = x / y;
-            return   result;
+            return;
+            
             } catch (Exception e) {
             e.printStackTrace();
         } finally {
             System.out.println("不管是否出现异常都执行");
         }
         System.out.println("3、除法计算结束。");
-        return   result;
-
-
     }
     public static void main(String[] args){
-           System.out.println( add());
+            add();
          }
 }
