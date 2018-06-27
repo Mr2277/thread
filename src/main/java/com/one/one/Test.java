5815d25f5dcc12027a7c1cf38a7dae55ae503fd0
sh <837544713@qq.com>
2018-06-27 16:29:17 +0800

    Test JVM args

---
+++ src/main/java/com/one/one/Test.java
@@ -0,0 +1,30 @@
+package com.one.one;
+class a{
+    private byte aByte[]=new byte[1024*1024];
+}
+class b{
+    private byte aByte[]=new byte[2*1024*1024];
+}
+public class Test {
+    public static void main(String[] args){
+       /*
+        int x=103;
+        for(int i=1;i<=3;i++){
+            //x=2*x+1;
+            x=(x-1)/2;
+        }
+        System.out.println(x);
+        */
+       //byte b[]=new byte[1024*1024];
+        /*
+       for(int i=0;i<3;i++){
+           if(i<2) {
+               new a();
+           }
+           else{
+               new b();
+           }
+       }
+       */
+    }
+}
