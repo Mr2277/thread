5fb3a29ddec589af1d6b7d282160473680f5f6f7
sh <837544713@qq.com>
2018-06-28 19:24:07 +0800

    Test JVM args

--- src/main/java/com/one/one/Test.java
+++ src/main/java/com/one/one/Test.java
@@ -3,7 +3,10 @@ class a{
     private byte aByte[]=new byte[1024*1024];
 }
 class b{
-    private byte aByte[]=new byte[2*1024*1024];
+    private byte aByte[]=new byte[10*1024*1024];
+}
+class c{
+    private byte aByte[]=new byte[5*1024*1024];
 }
 public class Test {
     public static void main(String[] args){
@@ -26,5 +29,16 @@ public class Test {
            }
        }
        */
+
+       // for(int i=0;i<2;i++){
+         //   new a();
+       // }
+       // new a();
+        //new b();
+        //new b();
+        for(int i=0;i<4;i++) {
+           // new c();
+         //   new c();
+        }
     }
 }
